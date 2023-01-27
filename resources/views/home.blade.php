81ad7d5c736d4a122c90e7b5ac728d5d0e95c100
Risky Armansyah <rarmnsyah787@gmail.com>
2023-01-27 11:55:46 +0700

    module 3
    belajar mengenai route, view. Lalu, memahami alur dari route php, dapat melakukan request ke route dari server, dapat membuat html sederhana.
    dapat mengirim variabel ke html melalui php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Web Belajar</title>
+</head>
+<body>
+    <h1>Ini adalah Home page</h1>
+    <a href="about">About</a>
+
+<script src = "js/coba.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Risky",
+        "email" => "rarmnsyah787@gmail.com"
+    ]);
+});
+
+Route::get('/form', function () {
+    return view('form');
 });
