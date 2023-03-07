943c57affd7ffa3ef5566f7a6d86fcb8de95dfd7
Risky Armansyah <rarmnsyah787@gmail.com>
2023-03-07 13:48:21 +0700

    update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-lg bg-primary navbar-dark ">
+<nav class="navbar navbar-expand-lg bg-primary navbar-light ">
     <div class="container">
         <a class="navbar-brand" href="/">YTTA</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-        </button>
+        </button> --}}
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="row justify-content-center">
+    <div class="row justify-content-center" >
         <div class="col-lg-4 mt-3">
             <main class="form-signin w-100 m-auto">
                 <form action="/register" method="post">
